df943305211772a880ab038b519ec09b746ea9fb
Martin <escorpmartin97@gmail.com>
2023-02-17 22:01:39 -0300

    feat: implementa get static props do next

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -1,14 +1,51 @@
 import { Avatar, Heading, Text } from '@martin-ignite-ui/react'
+import { GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
 import { Container, UserHeader } from './styles'
 
-export default function Schedule() {
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
   return (
     <Container>
       <UserHeader>
-        <Avatar src="https://github.com/MartinGBB.png" />
-        <Heading>Martin Brazón</Heading>
-        <Text>Dev Front End</Text>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
       </UserHeader>
     </Container>
   )
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24,
+  }
 }
