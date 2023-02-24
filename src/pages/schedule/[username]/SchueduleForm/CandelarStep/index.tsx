897b21261598db7a3341863a85329d481684a0b0
Martin <escorpmartin97@gmail.com>
2023-02-23 22:35:52 -0300

    feat: adiciona aba de abertura do horarios

--- src/pages/schedule/[username]/SchueduleForm/CandelarStep/index.tsx
+++ src/pages/schedule/[username]/SchueduleForm/CandelarStep/index.tsx
@@ -1,10 +1,13 @@
 import { Calendar } from '../../../../../components/Calendar'
-import { Container } from './style'
+import { Container, TimePicker } from './style'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+      {isDateSelected && <TimePicker />}
     </Container>
   )
 }
