48b7f1d9229893d5977b2d4588088f7d99835f5e
Martin <escorpmartin97@gmail.com>
2023-02-23 22:57:13 -0300

    style: estrutura dos botoes

--- src/pages/schedule/[username]/SchueduleForm/CandelarStep/style.ts
+++ src/pages/schedule/[username]/SchueduleForm/CandelarStep/style.ts
@@ -36,8 +36,50 @@ export const TimePicker = styled('div', {
   width: 280,
 })
 
-export const TimePickerHeader = styled(Text, {})
+export const TimePickerHeader = styled(Text, {
+  fontWeight: '$medium',
 
-export const TimePickerList = styled('div', {})
+  span: {
+    color: '$gray200',
+  },
+})
 
-export const TimePickerItem = styled('button', {})
+export const TimePickerList = styled('div', {
+  marginTop: '$3',
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+  gap: '$2',
+
+  '@media (max-width: 900px)': {
+    gridTemplateColumns: '2fr',
+  },
+})
+
+export const TimePickerItem = styled('button', {
+  border: 0,
+  background: '$gray600',
+  padding: '$2 0',
+  cursor: 'pointer',
+  color: '$gray100',
+  borderRadius: 'sm',
+  fontSize: '$sm',
+  lineHeight: '$base',
+
+  '&:last-child': {
+    marginBottom: '$6',
+  },
+
+  '&:disabled': {
+    background: 'none',
+    cursor: 'default',
+    opacity: 0.4,
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500',
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100',
+  },
+})
