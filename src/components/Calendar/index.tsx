c3ffeaa03fbc330db588c0afeeaac87dedef32c2
Martin <escorpmartin97@gmail.com>
2023-03-06 22:17:43 -0300

    feat: corrige erros diversos

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -78,7 +78,7 @@ export function Calendar() {
         return { date, disabled: false }
       }),
 
-      nextMontFillArray.map((date) => {
+      ...nextMontFillArray.map((date) => {
         return { date, disabled: true }
       }),
     ]
