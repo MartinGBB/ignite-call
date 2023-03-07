290e721e1a93b860b43ba0f757fb4d2083b751a2
Martin <escorpmartin97@gmail.com>
2023-03-07 19:04:11 -0300

    feat: exibindo horarios disponiveis

--- src/pages/schedule/[username]/SchueduleForm/CandelarStep/index.tsx
+++ src/pages/schedule/[username]/SchueduleForm/CandelarStep/index.tsx
@@ -11,9 +11,14 @@ import {
   TimePickerList,
 } from './style'
 
+interface Availability {
+  possibleTimes: number[]
+  availabileTimes: number[]
+}
+
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState(null)
+  const [availability, setAvailability] = useState<Availability | null>(null)
 
   const router = useRouter()
 
@@ -32,7 +37,7 @@ export function CalendarStep() {
         },
       })
       .then((response) => {
-        console.log(response)
+        setAvailability(response)
       })
   }, [selectedDate, username])
 
@@ -50,17 +55,16 @@ export function CalendarStep() {
             {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
           <TimePickerList>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>09:00h</TimePickerItem>
-            <TimePickerItem>10:00h</TimePickerItem>
-            <TimePickerItem>11:00h</TimePickerItem>
-            <TimePickerItem>12:00h</TimePickerItem>
-            <TimePickerItem>13:00h</TimePickerItem>
-            <TimePickerItem>14:00h</TimePickerItem>
-            <TimePickerItem>15:00h</TimePickerItem>
-            <TimePickerItem>16:00h</TimePickerItem>
-            <TimePickerItem>17:00h</TimePickerItem>
-            <TimePickerItem>18:00h</TimePickerItem>
+            {availability?.possibleTimes.map((hour) => {
+              return (
+                <TimePickerItem
+                  key={hour}
+                  disabled={!availability.availabileTimes.includes(hour)}
+                >
+                  {String(hour).padStart(2, '0')}
+                </TimePickerItem>
+              )
+            })}
           </TimePickerList>
         </TimePicker>
       )}
